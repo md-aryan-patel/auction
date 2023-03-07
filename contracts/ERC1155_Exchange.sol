a07cc0ad930554345e93ba6c7920eda3c575e423
Aryan Patel <aryan.patel@minddeft.net>
2023-03-07 17:43:24 +0530

    updated ERC1155 exchange

--- contracts/ERC1155_Exchange.sol
+++ contracts/ERC1155_Exchange.sol
@@ -4,19 +4,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Exchange {
-    mapping(address => uint256) Bids;
-    mapping(address => bool) isWinner;
+    mapping(uint256 => mapping(address => uint256)) Bids;
+    mapping(uint256 => mapping(address => bool)) isWinner;
 
     struct Sale {
         ERC1155 token;
         address owner;
         uint256 amount;
-        uint256 id;
         uint256 buyPrice;
         uint256 minBid;
     }
 
-    Sale public sale;
+    mapping(uint256 => Sale) public sale;
 
     function createAuction(
         address _token,
@@ -26,43 +25,64 @@ contract Exchange {
         uint256 _minBid
     ) public returns (bool) {
         ERC1155 token = ERC1155(_token);
+        require(address(sale[_id].token) == address(0), "Sale already exist");
         require(
             _amount <= token.balanceOf(msg.sender, _id),
             "User dont have enough to create auction"
         );
-        sale = Sale(token, msg.sender, _amount, _id, _buyPrice, _minBid);
+        sale[_id] = Sale(token, msg.sender, _amount, _buyPrice, _minBid);
         return true;
     }
 
-    function InstentBuy() public payable {
-        require(msg.value >= sale.buyPrice);
-        payable(sale.owner).transfer(msg.value);
-        sale.token.safeTransferFrom(sale.owner, msg.sender, sale.id, 1, "");
+    function InstentBuy(uint256 _id) public payable {
+        Sale memory currentSale = sale[_id];
+        require(msg.value >= currentSale.buyPrice);
+        payable(currentSale.owner).transfer(msg.value);
+        currentSale.token.safeTransferFrom(
+            currentSale.owner,
+            msg.sender,
+            _id,
+            1,
+            ""
+        );
     }
 
-    function placeBid() public payable returns (uint256) {
-        require(msg.value >= sale.minBid);
-        require(msg.sender != sale.owner, "Exchange Owner can't bid");
-        return Bids[msg.sender] += msg.value;
+    function placeBid(uint256 _id) public payable returns (uint256) {
+        Sale memory currentSale = sale[_id];
+        require(msg.value >= currentSale.minBid);
+        require(msg.sender != currentSale.owner, "Exchange Owner can't bid");
+        return Bids[_id][msg.sender] += msg.value;
     }
 
-    function declareWinners(address[] memory winners) public returns (uint256) {
-        require(msg.sender == sale.owner, "Exchange: Not Owner");
+    function declareWinners(
+        uint256 _id,
+        address[] memory winners
+    ) public returns (uint256) {
+        Sale memory currentSale = sale[_id];
+        require(msg.sender == currentSale.owner, "Exchange: Not Owner");
         for (uint256 i = 0; i < winners.length; i++) {
-            isWinner[winners[i]] = true;
+            isWinner[_id][winners[i]] = true;
         }
         return winners.length;
     }
 
-    function checkResult() public {
-        address transferTo;
-        if (isWinner[msg.sender] == true) {
-            sale.token.safeTransferFrom(sale.owner, msg.sender, sale.id, 1, "");
-            transferTo = sale.owner;
-            delete isWinner[msg.sender];
+    function checkResult(uint256 _id) public returns (address transferTo) {
+        Sale memory currentSale = sale[_id];
+        if (isWinner[_id][msg.sender] == true) {
+            currentSale.token.safeTransferFrom(
+                currentSale.owner,
+                msg.sender,
+                _id,
+                1,
+                ""
+            );
+            transferTo = currentSale.owner;
+            delete isWinner[_id][msg.sender];
         } else transferTo = msg.sender;
 
-        payable(transferTo).transfer(Bids[msg.sender] - Bids[msg.sender] / 100);
-        delete Bids[msg.sender];
+        payable(transferTo).transfer(
+            Bids[_id][msg.sender] - Bids[_id][msg.sender] / 100
+        );
+        delete Bids[_id][msg.sender];
     }
 }
